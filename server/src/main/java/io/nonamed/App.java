1fb1291b2715631444fca4c8f4f96908bca8e244
Changhwa Oh <changhwaoh.co@gmail.com>
2014-09-24 03:41:41 +0900

    bugfix(common) : @ResponseBody로 json 리턴시 한글깨지는 현상 수정

--- server/src/main/java/io/nonamed/App.java
+++ server/src/main/java/io/nonamed/App.java
@@ -2,8 +2,15 @@ package io.nonamed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+import java.nio.charset.Charset;
 
 @Configuration
 @ComponentScan
@@ -11,6 +18,20 @@ import org.springframework.context.annotation.Configuration;
 public class App {
 
     public static void main(String[] args) throws Exception {
+
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
     }
 }
